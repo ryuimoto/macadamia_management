15cdd6617401852ae735849bec641e30b9692176
井本竜 <>
2020-01-31 13:43:37 +0900

    設定画面　見た目完成した

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -4,41 +4,53 @@
 @endsection
 @section('contents')
   <div class="content-wrapper">
-    <div class="col-12 grid-margin stretch-card">
+    <div class="col-md-12 grid-margin stretch-card">
         <div class="card">
           <div class="card-body">
-            <h4 class="card-title">アカウント編集</h4>
-            <p class="card-description"> Basic form elements </p>
+            <h4 class="card-title">通知</h4>
+            <p class="card-description"> メール配信 </p>
             <form class="forms-sample">
-              <div class="form-group">
-                <label for="exampleInputName1">名前</label>
-                <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+              <div class="form-group row">
+                <div class="col-sm-12">
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label">月末に労働時間のお知らせをする</label>
+                        <div class="col-sm-4">
+                          <div class="form-check">
+                            <label class="form-check-label">
+                              <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" checked> 通知する </label>
+                          </div>
+                        </div>
+                        <div class="col-sm-5">
+                          <div class="form-check">
+                            <label class="form-check-label">
+                              <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios2" value="option2"> 通知しない </label>
+                          </div>
+                        </div>
+                    </div>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="exampleInputEmail3">ステータス</label>
-                <select class="form-control form-control-sm" id="exampleFormControlSelect3">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
+              <div class="form-group row">
+                <div class="col-sm-12">
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label">毎月27日に労働時間を確認するよう促す</label>
+                        <div class="col-sm-4">
+                          <div class="form-check">
+                            <label class="form-check-label">
+                              <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios1" value="" checked> 通知する </label>
+                          </div>
+                        </div>
+                        <div class="col-sm-5">
+                          <div class="form-check">
+                            <label class="form-check-label">
+                              <input type="radio" class="form-check-input" name="membershipRadios" id="membershipRadios2" value="option2"> 通知しない </label>
+                          </div>
+                        </div>
+                    </div>
+                </div>
               </div>
-              <div class="form-group">
-                <label for="exampleInputPassword4">メールアドレス</label>
-                <input type="email" class="form-control" id="exampleInputPassword4" placeholder="Email">
-              </div>
-              <div class="form-group">
-                <label for="exampleSelectGender">パスワード</label>
-                <input type="password" class="form-control" id="exampleInputPassword4" placeholder="パスワード">
-              </div>
-              <div class="form-group">
-                <label for="exampleSelectGender">パスワードの確認</label>
-                <input type="password" class="form-control" id="exampleInputPassword4" placeholder="パスワードの確認">
-              </div>
-              <button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
+              <button type="submit" class="btn btn-gradient-primary mr-2">編集する</button>
             </form>
           </div>
         </div>
-    </div>
+      </div>
 @endsection
